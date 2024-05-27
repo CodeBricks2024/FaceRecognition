868a8899836c936cd4fb44c43682a363a02f2c9c
songkyung <min.songkyung@gmail.com>
2024-05-27 14:32:35 +0900

    fastapi 적용

--- FaceRecognition/main.py
+++ FaceRecognition/main.py
@@ -8,10 +8,20 @@ import face_detector
 import coremltools as ct
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Flatten, MaxPooling2D, Conv2D
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+
+# FASTAPI 초기화
+app = FastAPI()
+
+# 라벨 매핑 (예시)
+label_map = {'개그맨': 0, '가수': 1, '배우': 2}
 
 w = 255
 h = 0
-output_filename = "Samples"
+
+# 샘플 이미지 디렉토리
+sample_dir = "Samples"
 
 # 샘플 이미지를 사용하여 모델 학습
 samples = []
@@ -20,6 +30,7 @@ results = []
 label_map = {'개그맨': 0, '가수': 1, '배우': 2}  # 라벨 매핑 예시
 
 data_dir = "Celeb Dataset"
+sample_file_path = ""
 
 
 # PIL 이미지를 NumPy 배열로 변환하는 함수
@@ -30,6 +41,7 @@ def pil_to_np(image):
 def preprocess_image(image_path, image_width, image_height):
     img = Image.open(image_path)
     img = img.resize((image_width, image_height))
+    img = np.expand_dims(img, axis=0)
     img = np.array(img)
     if img.shape[-1] == 4:  # PNG 이미지에서 alpha 채널 제거
         img = img[:, :, :3]
@@ -44,18 +56,16 @@ for directory in os.listdir(data_dir):
                 # face_detector.crop_face(joined)
 
                 first_file = os.listdir(joined)[0]
-                first_file_path = joined+"/"+first_file
-                if os.path.isfile(first_file_path):
+                sample_file_path = joined + "/" + first_file
+                if os.path.isfile(sample_file_path):
                         # 이미지 리사이징 및 전처리
-                        img = Image.open(first_file_path)
+                        img = Image.open(sample_file_path)
                         img = img.resize((w, int(w * (img.height / img.width))))
                         h = int(img.height)
                         samples.append(img)
                         labels.append(directory)
-                        img.save(os.path.join(output_filename, f"{directory}.jpg"))
+                        img.save(os.path.join(sample_dir, f"{directory}.jpg"))
 
-                        img = preprocess_image(image_path=first_file_path, image_width=img.width, image_height=img.height)
-                        print("firstfilecheck:", first_file_path)
 
                         # TODO: 개그맨, 가수, 배우별 디렉토리 라벨링
                         # labels.append(label_map[directory])
@@ -74,29 +84,52 @@ from sklearn.model_selection import train_test_split
 
 train_images, val_images, train_labels, val_labels = train_test_split(samples, labels, test_size=0.2, random_state=42)
 
-# train_images와 val_images를 PIL 이미지에서 NumPy 배열로 변환
+# train_images와 val_images를 PIL 이미지에서
+# NumPy 배열로 변환
 train_images = [pil_to_np(image) for image in train_images]
 val_images = [pil_to_np(image) for image in val_images]
 
-# DeepFace를 사용하여 유사도 계산
-for file in os.listdir("Samples"):
-        if file.endswith(".jpg"):
-                print("file check:", file)
-                result = DeepFace.verify("person8.jpg", f"Samples/{file}")
-                print(json.dumps(result, indent=2))
-                results.append(result)
-                if result['verified']:
-                        print("This person looks exactly like", file.split(".")[0])
-                        break
-                if smallest_distance is None:
-                        smallest_distance = (file.split(".")[0], result['distance'])
-                else:
-                        smallest_distance = (file.split(".")[0], result['distance']) if result['distance'] < smallest_distance[1] else smallest_distance
-else:
-        print(f"No exact match found! Closest match is {smallest_distance[0]}")
 
-# result = DeepFace.verify("person1.jpg", f"Samples/Angelina Jolie.jpg ")
-# distance: 두 이미지가 얼마나 동떨어져있는지 확인 (distance가 낮으면 두 이미지가 유사하다는 의미)
+
+@app.post("/compare")
+async def compare(file: UploadFile = File(...)):
+    img = Image.open(file.file)
+    processed_img = preprocess_image(image_path=sample_file_path, image_width=img.width, image_height=img.height)
+    print("firstfilecheck:", sample_file_path)
+
+    # 모델을 사용하여 특징 벡터 추출
+    features = model.predict(processed_img)
+
+    smallest_distance = None
+    closest_match = None
+
+    # 유사도 계산 함수
+    # DeepFace를 사용하여 유사도 계산
+    for file in os.listdir(sample_dir):
+        if file.endswith(".jpg") or file.endswith(".png"):
+            print("file check:", file)
+            result = DeepFace.verify("person8.jpg", f"Samples/{file}")
+            print(json.dumps(result, indent=2))
+            results.append(result)
+            if result['verified']:
+                print("This person looks exactly like", file.split(".")[0])
+                closest_match = file.split(".")[0]
+                break
+            if smallest_distance is None:
+                smallest_distance = (file.split(".")[0], result['distance'])
+            else:
+                smallest_distance = (file.split(".")[0], result['distance']) if result['distance'] < smallest_distance[
+                    1] else smallest_distance
+
+    else:
+        print(f"No exact match found! Closest match is {smallest_distance[0]}")
+        closest_match = smallest_distance[0]
+
+    # distance: 두 이미지가 얼마나 동떨어져있는지 확인 (distance가 낮으면 두 이미지가 유사하다는 의미)
+
+    return JSONResponse(content={"closest_match":closest_match, "distance":smallest_distance})
+
+
 
 
 samples = np.array(samples)
@@ -140,14 +173,21 @@ model.summary()
 # ct.ImageType의 name 매개변수를 conv2d_16_input으로 설정하여 입력 텐서의 이름을 모델의 실제 입력 레이어와 일치
 input_name = model.input_names[0]  # 첫 번째 입력 텐서의 이름 가져오기 (conv2d_16_input)
 
+
+
 # Core ML 모델로 변환
 # inputs 매개변수를 사용하여 입력 형식을 이미지로 설정
-image_input = ct.converters.mil.input_types.ImageType(name=input_name, shape=(1, w, w, 3))
-coreml_model = ct.convert(
-    model,
-    inputs = [image_input],
-    convert_to="mlprogram"
-)
+# image_input = ct.converters.mil.input_types.ImageType(name=input_name, shape=(1, w, w, 3))
+# coreml_model = ct.convert(
+#     model,
+#     inputs = [image_input],
+#     convert_to="mlprogram"
+# )
+#
+#
+# coreml_model.save("FaceFinder")
 
-
-coreml_model.save("FaceFinder")
+# 실행
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
